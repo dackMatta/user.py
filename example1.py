1d49727ac95dd0e9877c0e6a2ed1dde6ab0c12be
Entrix Makambi <160459342+dackMatta@users.noreply.github.com>
2024-02-24 13:32:19 +0300

    Add files via upload

---
+++ example1.py
@@ -0,0 +1,20 @@
+is_hot=True
+is_cold=True
+
+if is_hot:
+    print("it's a hot day ")
+    print("drink water ")
+elif is_cold:
+    print("it's a cold day")
+    print("wear warm clothes ")
+else:
+    print("it's a lovely day")
+print("enjoy your day ")
+
+
+
+
+
+
+
+
