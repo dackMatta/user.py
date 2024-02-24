f3baa5b7013b1c8e0e534a0cdc04cf8c2cc07dca
Entrix Makambi <160459342+dackMatta@users.noreply.github.com>
2024-02-24 13:30:50 +0300

    Create user.py

---
+++ user.py
@@ -0,0 +1,6 @@
+name= input('what is your name')
+age=input('how old are you')
+location=input('where do u stay')
+print("hello a'm " + name)
+print("i'm " + age +"year's old ")
+print("and live in " + location)
